7f8da2377741daf94db01dc98691c9de0797f1c2
Kaysera <folez97@gmail.com>
2017-05-31 11:48:43 +0200

    commit

--- php/dbconntest.php
+++ php/dbconntest.php
@@ -8,14 +8,14 @@
 		die('Error: no se pudo conectar con la base de datos');
 	}
 	mysql_select_db($database);
-	$team = 'Equipo1';
-	$track='Pista1';
+	$team = $_GET['Equipo'];
+	$track=$_GET['Circuito'];
 	$red=2;
 	$green=2;
 	$blue=2;
 	$order=2;
 	$base=1;
-	$laptime='00:30:00';	
+	$laptime=$_GET['Tiempo'];	
 	$sql="INSERT INTO `REGISTROS` (`TEAM`,`TRACK`,`RED`,`GREEN`,`BLUE`,`ORDER`,`BASE`,`LAP_TIME`) VALUES (\"".$team."\",\"".$track."\",$red,$green,$blue,$order,$base,\"".$laptime."\")";	
 	if(!(mysql_query($sql))){
 						$response['success']=0;
