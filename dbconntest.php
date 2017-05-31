dd127d8977373b56de5e972aae358a0204e91b26
Kaysera <folez97@gmail.com>
2017-05-31 11:00:51 +0200

    Commit

---
+++ dbconntest.php
@@ -0,0 +1,28 @@
+<?php
+	$dbhost='localhost';
+	$dbuser='root';
+	$dbpass='dronechallenge';
+	$database='prueba';
+	$reponse=array();
+	if(!($iden=mysql_connect($dbhost,$dbuser,$dbpass))){
+		die('Error: no se pudo conectar con la base de datos');
+	}
+	mysql_select_db($database);
+	$team = 'Equipo1';
+	$track='Pista1';
+	$red=2;
+	$green=2;
+	$blue=2;
+	$order=2;
+	$base=1;
+	$laptime='00:30:00';
+	$sql="INSERT INTO REGISTROS (TEAM,TRACK,RED,GREEN,BLUE,ORDER,BASE,LAP_TIME) VALUES ($team,$track,$red,$green,$blue,$order,$base,$laptime)";	
+	if(!(mysql_query($sql))){
+						$response['success']=0;
+						die('Error: no se pudo ejecutar la consulta');
+					}else{
+						$response['success']=1;
+					}
+	mysql_close($iden);
+?>
+
