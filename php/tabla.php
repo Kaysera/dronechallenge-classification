cd0ec939e3cb8f11b2bb410e27867f0f13d0f3cf
NecaX <pablo_1997_olivas@hotmail.es>
2017-06-06 11:36:18 +0200

    Equipo

---
+++ php/tabla.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Clasificaciones ESII Drone Challenge</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="../css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<script src="../js/bootstrap.min.js"></script>
+	</head>
+	
+	<style>
+	.content {
+		max-width: 700px;
+		margin: auto;
+	}
+	</style>
+	
+	<body>
+	
+		<div class="jumbotron text-center">
+			<h2>Tiempos por Equipo ESII Drone Challenge</h2>
+			
+			<div class="content">
+				<div class="container-fluid">
+					<div >
+						<div class=" col-sm-12 col-xs-12">
+							<img  src="../logos/todos.png" alt="todos" style="width:100%">
+						</div>	
+					</div>
+				</div>	
+			</div>
+		</div>
+		
+		<div class="container">
+		  <button type="button" class="btn btn-info" data-toggle="collapse" data-target="#demo">Leyenda</button>
+		  <div id="demo" class="collapse">
+			<p> <p> 
+			PC: Puntuacion del circuito<p>
+			TC: Tiempo del circuito<p>
+			PT: Puntuacion total<p> 
+			TT: Tiempo total<p>
+		  </div>
+		</div>
+		
+		<div class="content">
+			<div class="container-fluid">
+				<div class="table-responsive">
+				<table class="table table-condensed table-striped">
+					<thead>
+						<tr>
+							<th>Equipo</th>
+							<th>PC 1</th>
+							<th>TC 1</th>
+							<th>PC 2</th>
+							<th>TC 2</th>
+							<th>PC 3</th>
+							<th>TC 3</th>
+							<th>PT 1</th>
+							<th>TT 1</th>
+						</tr>
+					</thead>
+					<tbody>
+						
+					</tbody>
+				</table>
+				</div>
+			</div>
+		</div>
+	 			
+	</body>
+</html>
+
